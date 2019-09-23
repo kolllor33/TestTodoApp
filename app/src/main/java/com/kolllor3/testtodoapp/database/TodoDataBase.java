155f1b30ad3b91ff5318f1e924d934e1ab3d3fca
Kobe Bogaerts Desk <kolllor3@gmail.com>
2019-09-23 20:01:09 +0200

    set exportSchema to false in room so the warning is gone and don't need to export

--- app/src/main/java/com/kolllor3/testtodoapp/database/TodoDataBase.java
+++ app/src/main/java/com/kolllor3/testtodoapp/database/TodoDataBase.java
@@ -7,7 +7,7 @@ import com.kolllor3.testtodoapp.model.TodoItem;
 
 //init the db by adding which entities(model classes) are in the database and the iteration of the database
 //this needs to be increased every time the schema of the database changes
-@Database(entities = {TodoItem.class}, version = 1)
+@Database(entities = {TodoItem.class}, version = 1, exportSchema = false)
 public abstract class TodoDataBase extends RoomDatabase {
     public abstract TodoItemDao getTodoItemDao();
 }
